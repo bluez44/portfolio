4012657ffedc7222326599390cfb75c588d36c05
Tom <vlqvinh444@gmail.com>
2026-07-08 11:01:12 +0700

    chore: add speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -27,6 +28,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <SpeedInsights />
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
